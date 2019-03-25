d72f5af1650ee0a570e0b85bd4e30dd79c3894c3
Edinson Gutierrez <egutierrez.9615@gmail.com>
2019-03-24 20:33:02 -0500

    add support php7.2 mysql

--- instalador.sh
+++ instalador.sh
@@ -8,7 +8,7 @@ echo "Instalando Modulos de PHP"
 sudo add-apt-repository ppa:ondrej/php
 
 sudo apt install php-common php-curl php-json php-readline php-fpm php-cli php-xml php-mcrypt php-zip php-mbstring php-gd -y
-sudo apt install php7.2 libapache2-mod-php7.2 php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-cli php7.2-zip -y
+sudo apt install php7.2 libapache2-mod-php7.2 php7.2-pdo php7.2-mysql php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-cli php7.2-zip -y
 
 sudo phpenmod mcrypt
 
